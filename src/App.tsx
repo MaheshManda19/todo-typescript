c29f377e451db3b6233a6ecc2bcd63a184c473a8
Maheshmanda19 <mahesh.r@marketsimplified.com>
2023-10-18 11:15:36 +0530

    message

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<string[]>([]);
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+
+  const handleTodo = () => {
+    if (todo.trim() !== "") {
+      if (editIndex !== null) {
+        // If editing an existing item
+        const updatedTodos = [...todos];
+        updatedTodos[editIndex] = todo;
+        setTodos(updatedTodos);
+        setEditIndex(null);
+      } else {
+        // If adding a new item
+        setTodos([...todos, todo]);
+      }
+      setTodo("");
+    }
+  };
+
+  const handleEdit = (index: number) => {
+    setTodo(todos[index]);
+    setEditIndex(index);
+  };
+
+  const handleRemove = (index: number) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+    setEditIndex(null);
+  };
+
+  const onDragStart = (e: React.DragEvent<HTMLLIElement>, index: number) => {
+    e.dataTransfer.setData("index", index.toString());
+  };
+
+  const onDragOver = (e: React.DragEvent<HTMLLIElement>) => {
+    e.preventDefault();
+  };
+
+  const onDrop = (e: React.DragEvent<HTMLLIElement>, targetIndex: number) => {
+    const sourceIndex = Number(e.dataTransfer.getData("index"));
+    const updatedTodos = [...todos];
+    const [draggedTodo] = updatedTodos.splice(sourceIndex, 1);
+    updatedTodos.splice(targetIndex, 0, draggedTodo);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={handleTodo}>
+        {editIndex !== null ? "Update" : "Add"}
+      </button>
+      <ul className="todo-list">
+        {todos.map((item, index) => (
+          <li
+            key={index}
+            draggable
+            onDragStart={(e) => onDragStart(e, index)}
+            onDragOver={(e) => onDragOver(e)}
+            onDrop={(e) => onDrop(e, index)}
+            onDragEnd={(e) => e.preventDefault()}
+          >
+            {item}
+            <button onClick={() => handleEdit(index)}>Edit</button>
+            <button onClick={() => handleRemove(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
